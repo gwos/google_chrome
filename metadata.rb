6c27f2776e18f79e77b18bd080998527e18e7db6
anant jain <anant.jain@optimusinfo.com>
2013-05-17 13:07:59 +0530

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Optimusinfo"
+maintainer_email "gwoptimusqa@gmail.com"
+license          "Apache 2.0"
+description      "Installs Google Chrome"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends          "apt" 
+supports         "ubuntu"
